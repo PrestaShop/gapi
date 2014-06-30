c80235a4dbf08dfb7e3a2fe1ec884c6419ef1c5b
Francois Gaillard <francois.gaillard@prestashop.com>
2014-06-30 17:26:01 +0200

    GApi : Version updated (v1.0.0)

--- gapi.php
+++ gapi.php
@@ -33,7 +33,7 @@ class Gapi extends Module
 	{
 		$this->name = 'gapi';
 		$this->tab = 'administration';
-		$this->version = '0.14';
+		$this->version = '1.0.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
