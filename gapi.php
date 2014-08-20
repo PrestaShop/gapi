92dba010151d93bebbf119c663ccf5aee3d6c01c
Jerome Nadaud <jerome.nadaud@gmail.com>
2014-08-20 10:32:18 +0200

    //Version update

--- gapi.php
+++ gapi.php
@@ -33,7 +33,7 @@ class Gapi extends Module
 	{
 		$this->name = 'gapi';
 		$this->tab = 'administration';
-		$this->version = '1.0.0';
+		$this->version = '1.0.1';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 		$this->bootstrap = true;
